076f85931fbf6f2df877b0b952ce1bf5907e0b00
NurulAfsar96 <nurulafsar.jony96@gmail.com>
2023-09-17 12:14:10 +0600

    Table handling update

--- cypress/e2e/Basics/Table.cy.js
+++ cypress/e2e/Basics/Table.cy.js
@@ -22,7 +22,7 @@ beforeEach(function(){
 
 })
 
-it("Table list Test", function(){
+it.skip("Table Header Column value Test", function(){
 
     // Table Header Table Data(Column) Testing 
     cy.get('table[class="table table-bordered table-hover"]>thead>tr>td').should('have.length', '7')
@@ -30,9 +30,30 @@ it("Table list Test", function(){
     cy.get('table[class="table table-bordered table-hover"]>thead>tr>td:nth-child(3)').should('have.text', "E-Mail")
     cy.get('table[class="table table-bordered table-hover"]>thead>tr>td:nth-child(5)').should('have.text', "Status")
 
-    // Table Body Table Data value(Column) Testing
-    cy.get('table[class="table table-bordered table-hover"]>tbody>tr:nth-child(6)>td:nth-child(3)').should('have.text', "ghh56@gmail.com")
+})
 
+it.skip("Table Body Column value Test", function(){
+    // Table Body Table Data value(Column) Testing
+    cy.get('table[class="table table-bordered table-hover"]>tbody>tr:nth-child(6)>td:nth-child(3)').should('have.text', "ghh56@gmail.comgit")
+    //cy.get('table[class="table table-bordered table-hover"]>tbody>tr:nth-child(6)>td:nth-child(3)').contains("ghh56@gmail.comgit")
+})
+
+it.only("Read All Values", function(){
+
+    cy.get('table[class="table table-bordered table-hover"]>tbody>tr').each(function($row, index, $rows){
+    cy.wrap($row).within(function(){
+
+        cy.get("td").each(function($col, index, $cols){
+
+            if($col.text()=='olaola@das.com')
+             {  cy.log($col.text()) }
+             //cy.log($col.text())
+
+        })
+            
+})
+
+ })
 
 })
 
